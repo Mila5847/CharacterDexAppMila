d11b12977a36a8c49d309e4c26c1bef17c19e9b9
mila <mila.kehayova@abv.bg>
2023-11-08 16:45:59 -0500

    Initial Commit

---
+++ CharacterDexAppMila/CharacterDexAppMilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDexAppMilaApp.swift
+//  CharacterDexAppMila
+//
+//  Created by macuser on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct CharacterDexAppMilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
